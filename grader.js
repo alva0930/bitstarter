4c4cd2f7c01fa3016608bfbad06e299aa407a72b
Alva Lin <alva0930@gmail.com>
2013-07-08 11:06:11 +0000

    Updated grader.js to add --url option

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require("restler");
+var util = require("util");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -48,9 +50,8 @@ var loadChecks = function(checksfile)
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile)
+var checkPresence = function($, checksfile)
 {
-    $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks)
@@ -68,15 +69,38 @@ var clone = function(fn)
     return fn.bind({});
 };
 
+var response2console = function(result, response)
+{
+    if (result instanceof Error)
+    {
+        console.error('Error: ' + util.format(response.message));
+    }
+    else
+    {
+        var checkJson = checkPresence(cheerio.load(result), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+};
+
 if (require.main == module)
 {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL of index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url)
+    {
+        restler.get(program.url).on('complete', response2console);
+    }
+    else
+    {
+        var checkJson = checkPresence(cheerioHtmlFile(program.file), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 }
 else
 {
